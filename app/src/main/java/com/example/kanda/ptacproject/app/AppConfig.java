9987d2fadab714954aebedf239b825876101f3e2
aphisit <aphisit.jankiaw@gmail.com>
2016-09-12 00:30:22 +0700

    icon marker

--- app/src/main/java/com/example/kanda/ptacproject/app/AppConfig.java
+++ app/src/main/java/com/example/kanda/ptacproject/app/AppConfig.java
@@ -11,6 +11,8 @@ public class AppConfig {
     public static String URL_MARKER_LIST = "http://www.ptacproject.com/android_login_api/getallmarker.php";
     public static String URL_SEARCH_FRIEND = "http://www.ptacproject.com/android_login_api/searchfriend.php";
     public static String URL_CHECK_REQUEST = "http://www.ptacproject.com/android_login_api/checkrequest.php";
+    public static String URL_ADD_MARKER = "http://www.ptacproject.com/android_login_api/addMarker.php";
+
     public static String URL_UPDATE_STATUS = "http://www.ptacproject.com/android_login_api/updatestatus.php";
     public static String URL_REMOVE_REQUEST = "http://www.ptacproject.com/android_login_api/removerequest.php";
 
